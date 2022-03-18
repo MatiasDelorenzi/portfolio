8d1de03fb95ec57ce3fa509f2624a7c722300edf
Matias Delorenzi <matiasdelorenzi1997@gmail.com>
2022-03-18 00:09:55 -0300

    Portfolio list

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,56 @@
+import { useState } from 'react'
+import PortfolioList from '../portfolioList/PortfolioList'
 import './portfolio.scss'
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured")
+  const list = [
+    { id: 'featured', title: 'Featured' },
+    { id: 'web', title: 'Web App' },
+    { id: 'mobile', title: 'Mobile App' },
+    { id: 'design', title: 'Design' },
+    { id: 'content', title: 'Content' }
+  ]
+
   return (
-    <div className='portfolio' id='portfolio'></div>
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected} 
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://icdn.digitaltrends.com/image/digitaltrends/best-mobile-banking-tips-citi-7852-1053x702.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
   )
 }
