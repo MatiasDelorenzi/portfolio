22c93f1553ec97aeb0d6aae8a32cf2f7c84d52db
Matias Delorenzi <matiasdelorenzi1997@gmail.com>
2022-03-19 01:54:05 -0300

    Add socials to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,13 +15,39 @@ export default function Contact() {
         <img src="assets/shake.svg" alt="" />
       </div>
       <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Email' />
-          <textarea placeholder='Message'></textarea>
-          <button type='submit'>Send</button>
-          {message && <span>Thanks! I will reply ASAP</span>}
-        </form>
+        <div className="contactForm">
+
+          <h2>Contact Me</h2>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Email' />
+            <textarea placeholder='Message'></textarea>
+            <button type='submit'>Send</button>
+            {message && <span>Thanks! I will reply ASAP</span>}
+          </form>
+
+        </div>
+
+        <div className="socials">
+          <h2>My socials.</h2>
+          <div className="logos">
+            <a href="https://github.com/MatiasDelorenzi" rel="noreferrer" target="_blank">
+              <div className='imageContainer'>
+                <img src="assets/github.png" className='linkedin' alt='' />
+              </div>
+            </a>
+            <a href="https://www.linkedin.com/in/matias-delorenzi/" rel="noreferrer" target="_blank">
+              <div className='imageContainer'>
+                <img src="assets/linkedin.png" className='linkedin' alt='' />
+              </div>
+            </a>
+            <a href="https://www.instagram.com/matias.delorenzi/?hl=es-la" rel="noreferrer" target="_blank">
+              <div className='imageContainer'>
+                <img src="assets/instagram.png" className='linkedin' alt='' />
+              </div>
+            </a>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
